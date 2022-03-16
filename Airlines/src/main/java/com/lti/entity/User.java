eed0a3ce2310f9f7874f42d050386a53e428e9e0
Dhanunjaya-kadali <kadalidhanunjaya99@gmail.com>
2022-03-16 22:32:14 +0530

    first commit

--- Airlines/src/main/java/com/lti/entity/User.java
+++ Airlines/src/main/java/com/lti/entity/User.java
@@ -1,5 +1,34 @@
 package com.lti.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tbl_user")
 public class User {
+	@Id
+	@GeneratedValue
+	private int u_id;
+	private String f_name;
+	private String l_name;
+	private int u_age;
+	private String u_gender;
+	private String u_email;
+	private String u_address;
+	private String u_pass;
+	
+	@OneToMany(mappedBy = "user")
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
